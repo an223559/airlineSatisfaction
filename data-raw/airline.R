e6a3b81ec4788e7ccb6ba7a310583189b32f49cc
Anqi Zhen <an223559@dal.ca>
2026-04-10 11:29:54 -0300

    Add project report vignette based on Rose Vafadar's draft and analysis

--- data-raw/airline.R
+++ data-raw/airline.R
@@ -1,3 +1,4 @@
-## code to prepare `airline` dataset goes here
-
+airline <- read.csv("data-raw/cleaned_airline_dataset.csv")
+airline$arrival_delay <- as.numeric(airline$arrival_delay)
+airline$satisfaction <- trimws(airline$satisfaction)
 usethis::use_data(airline, overwrite = TRUE)
