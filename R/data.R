a58221158fe37324ebf010a5c4b967f2d2826f47
Anqi Zhen <an223559@dal.ca>
2026-04-10 12:25:15 -0300

    Add documentation for airline dataset

---
+++ R/data.R
@@ -0,0 +1,36 @@
+#' Airline Passenger Satisfaction Dataset
+#'
+#' A sample of 1,000 airline passengers with service quality ratings,
+#' flight information, and satisfaction outcomes.
+#'
+#' @format A data frame with 1,000 rows and 25 variables:
+#' \describe{
+#'   \item{id}{Passenger ID}
+#'   \item{gender}{Gender of the passenger (Male/Female)}
+#'   \item{customer_type}{Customer type (Loyal Customer/Disloyal Customer)}
+#'   \item{age}{Age of the passenger in years}
+#'   \item{type_of_travel}{Purpose of travel (Business travel/Personal travel)}
+#'   \item{class}{Travel class (Business/Eco/Eco Plus)}
+#'   \item{flight_distance}{Flight distance in kilometres}
+#'   \item{inflight_wifi_service}{Inflight wifi service rating (0-5)}
+#'   \item{departure_arrival_time_convenient}{Departure/arrival time convenience rating (0-5)}
+#'   \item{ease_of_online_booking}{Ease of online booking rating (0-5)}
+#'   \item{gate_location}{Gate location rating (0-5)}
+#'   \item{food_and_drink}{Food and drink rating (0-5)}
+#'   \item{online_boarding}{Online boarding rating (0-5)}
+#'   \item{seat_comfort}{Seat comfort rating (0-5)}
+#'   \item{inflight_entertainment}{Inflight entertainment rating (0-5)}
+#'   \item{onboard_service}{Onboard service rating (0-5)}
+#'   \item{leg_room_service}{Leg room service rating (0-5)}
+#'   \item{baggage_handling}{Baggage handling rating (0-5)}
+#'   \item{checkin_service}{Check-in service rating (0-5)}
+#'   \item{inflight_service}{Inflight service rating (0-5)}
+#'   \item{cleanliness}{Cleanliness rating (0-5)}
+#'   \item{departure_delay}{Departure delay in minutes}
+#'   \item{arrival_delay}{Arrival delay in minutes}
+#'   \item{satisfaction}{Satisfaction level (satisfied/neutral or dissatisfied)}
+#'   \item{satisfied_or_no}{Binary satisfaction indicator (1 = satisfied, 0 = neutral or dissatisfied)}
+#' }
+#' @source Kaggle: Airline Passenger Satisfaction Dataset (teejmahal20)
+#' \url{https://www.kaggle.com/datasets/teejmahal20/airline-passenger-satisfaction}
+"airline"
